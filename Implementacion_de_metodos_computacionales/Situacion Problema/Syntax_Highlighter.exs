4b7c6a35d6aa1f9a429522aa01a06b967d460a7a
Saikou17 <a01783208@itesm.mx>
2023-05-30 21:08:26 -0600

    Documentacion,Python;Syntax

--- Implementacion_de_metodos_computacionales/Situacion Problema/Syntax_Highlighter.exs
+++ Implementacion_de_metodos_computacionales/Situacion Problema/Syntax_Highlighter.exs
@@ -4,15 +4,19 @@ defmodule Syntax_Highlighter do
   Identificamos los tokens para las expresiones regulares.
   """
   p_reservadas = ~r/\b(import|def|if|len|sys|argv|with|open|as|file|read|print|elif|except|as|else|and|assert|break|class|contine|del|exec|finally|for|from|global|in|is|lambda|not|or|pass|raise|return|try|while|yield|open)\b/
+  op_aritmeticos = ~r/\B(\+|\-|\*{1,2}|\/{1,2}|\%)\B/
+  op_relacionales = ~r/\B(\>=?|\<=?|\={1,2}|\!=)\B/
+  op_bit_a_bit = ~r/\&|\||\^|\<{1,2}|>{1,2}|\~/
+  op_asignacion = ~r(\+|\-|\|\/+|\%|\{1,2}|\<{1,2}|\>{1,2}|\&|\||\^|\?|\=)
+  op_logicos = ~r/\b(and|or|not)\b/
+  numeros = ~r/\b(\d.(\d)*|\d)\b/
+  booleanos = ~r/\b(True|False|None)\b/
   strings = ~r/\".*\"/
-  numeros = ~r/[0-9]+/
-  op_aritmeticos = ~r \B(\+|\-|\*{1,2}|\/{1,2}|\%)\B
-  op_relacionales = ~r \B(\>=?|\<=?|\={1,2}|\!=)\B
-  op_bit_a_bit = ~r \&|\||\^|\<{1,2}|>{1,2}|\~
-  identificadores = ~r/[a-zA-Z][a-zA-Z0-9_]*/
+  estructuras = ~r/(\[.*\]|\(.*\)|\{.*\})/
+  identificadores = ~r/([a-zA-Z]+(\_|\d)*)/
   comentarios = ~r/\/\/.*$|\/\*.*\*\//
   espacios = ~r/\s+/
-  asignacion = ~r(\+|\-|\|\/+|\%|\{1,2}|\<{1,2}|\>{1,2}|\&|\||\^|\?|\=)
+  tokens = (p_reservadas|op_aritmeticos|op_relacionales|op_bit_a_bit|op_asignacion|op_logicos|numeros|booleanos|strings|estructuras|identificadores|comentarios|espacios)
 
  # Funcions para leer un archivo y obtener los tokens
   def read_file(in_python_file, out_python_file) do # Funcion para leer un archivo y obtener los tokens
@@ -24,24 +28,21 @@ defmodule Syntax_Highlighter do
   end
 
 
-  def tokens_regular_expresions(line) do: do_tokens(line, []) # Funcion para obtener los tokens de una linea
+  def tokens_regular_expresions(line), do: do_tokens(_line,Regex.scan(tokens,line),"") # Funcion para obtener los tokens de una line
 
-  def do_tokens(line, ""), do
-    cond do
-      Regex.match?(p_reservadas, line) -> # Si es una palabra reservada
-        current = Regex.replace(p_reservadas, line, "<span style=\"color:blue\">\\1</span>") # La reemplazamos por un span azul
-        do_tokens(current, []) # Volvemos a llamar a la funcion
+  def do_tokens(_line,[],res), do: res #Caso base: Aqui detenemos la recursion
+  def do_tokens(line,[head|tail],res) when Regex.match?(p_reservadas,line), do: do_tokens(_line,tail,Enum.join("<span class=reservadas>"head"</span>",res))
+  def do_tokens(line,[head|tail],res) when Regex.match?(op_aritmeticos,line), do: do_tokens(_line,tail,Enum.join("<span class=aritmeticos>"head"</span>",res))
+  def do_tokens(line,[head|tail],res) when Regex.match?(op_relacionales,line), do: do_tokens(_line,tail,Enum.join("<span class=relacionales>"head"</span>",res))
+  def do_tokens(line,[head|tail],res) when Regex.match?(op_bit_a_bit,line), do: do_tokens(_line,tail,Enum.join("<span class=bitabit>"head"</span>",res))
+  def do_tokens(line,[head|tail],res) when Regex.match?(op_asignacion,line), do: do_tokens(_line,tail,Enum.join("<span class=asignacion>"head"</span>",res))
+  def do_tokens(line,[head|tail],res) when Regex.match?(op_logicos,line), do: do_tokens(_line,tail,Enum.join("<span class=logicos>"head"</span>",res))
+  def do_tokens(line,[head|tail],res) when Regex.match?(numeros,line), do: do_tokens(_line,tail,Enum.join("<span class=numeros>"head"</span>",res))
+  def do_tokens(line,[head|tail],res) when Regex.match?(booleanos,line), do: do_tokens(_line,tail,Enum.join("<span class=booleanos>"head"</span>",res))
+  def do_tokens(line,[head|tail],res) when Regex.match?(strings,line), do: do_tokens(_line,tail,Enum.join("<span class=strings>"head"</span>",res))
+  def do_tokens(line,[head|tail],res) when Regex.match?(estructuras,line), do: do_tokens(_line,tail,Enum.join("<span class=estructuras>"head"</span>",res))
+  def do_tokens(line,[head|tail],res) when Regex.match?(identificadores,line), do: do_tokens(_line,tail,Enum.join("<span class=identificadores>"head"</span>",res))
+  def do_tokens(line,[head|tail],res) when Regex.match?(comentarios,line), do: do_tokens(_line,tail,Enum.join("<span class=comentarios>"head"</span>",res))
+  def do_tokens(line,[head|tail],res) when Regex.match?(espacios,line), do: do_tokens(_line,tail,Enum.join("<span class=espacios>"head"</span>",res))
 
-
-
-  # defp do_tokens("", tokens), do:
-
-  #   cond do
-  #     Regex.match?(p_reservadas, tokens) ->
-  #       current = Regex.replace(p_reservadas, tokens, "<span style=\"color:blue\">\\1</span>")
-  #       do_tokens(current, [])
-  #     Regex.match?(strings, tokens) ->
-  #       current = Regex.replace(strings, tokens, "<span style=\"color:green\">\\1</span>")
-  #       do_tokens(current, [])
-  end
 end
