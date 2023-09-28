1d562288c8b3fa004c7f37b9b94509bd5732fadd
Daniel <a01782985@tec.mx>
2023-09-28 17:29:25 -0600

    Los tickets aparecen en la App

--- ReactAppWeb/Frontend/proyectoprincipal/src/App.tsx
+++ ReactAppWeb/Frontend/proyectoprincipal/src/App.tsx
@@ -1,11 +1,30 @@
-import { Admin, Resource, ShowGuesser} from "react-admin";
+import { Admin, Resource, ShowGuesser } from "react-admin";
 import { dataProvider } from "./dataProvider";
 import { PostList, PostEdit, PostCreate } from "./posts";
 import { UserList } from "./users";
+import { TicketsList } from "./tickets";
 import PostIcon from "@mui/icons-material/Book";
 import UserIcon from "@mui/icons-material/Group";
 
-export const App = () => <Admin dataProvider={dataProvider}>
-  <Resource name="users" list={UserList} show={ShowGuesser} recordRepresentation="name" />
-  <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate}/>
-</Admin>;
+export const App = () => (
+  <Admin dataProvider={dataProvider}>
+    <Resource
+      name="users"
+      list={UserList}
+      show={ShowGuesser}
+      recordRepresentation="name"
+    />
+    <Resource
+      name="tickets"
+      list={TicketsList}
+      show={ShowGuesser}
+      recordRepresentation="name"
+    />
+    <Resource
+      name="posts"
+      list={PostList}
+      edit={PostEdit}
+      create={PostCreate}
+    />
+  </Admin>
+);
