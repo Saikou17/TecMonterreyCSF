ab672900d3ef889cf7ecec0a265b452c76422a5c
Juan Pablo Moreno <jpmoreno98@gmail.com>
2023-10-10 10:46:29 -0600

    Creando la base del theme

---
+++ ReactAppWeb/Frontend/proyectoprincipal/src/Theme.tsx
@@ -0,0 +1,16 @@
+import { Admin, defaultTheme } from "react-admin";
+
+
+const myTheme = {
+
+    ...defaultTheme,
+    
+    palette: {
+        primary : '#FCFAFA',
+        secondary: '#C8D3D5',
+        third: '#0CCA4A',
+    },
+
+};
+
+export default myTheme;
