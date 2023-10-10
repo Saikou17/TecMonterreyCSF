ca7775c7d93f823c30301561c13780ddb4b5f251
Juan Pablo Moreno <jpmoreno98@gmail.com>
2023-10-10 11:26:40 -0600

    Iconos

--- ReactAppWeb/Frontend/proyectoprincipal/src/App.tsx
+++ ReactAppWeb/Frontend/proyectoprincipal/src/App.tsx
@@ -8,12 +8,12 @@ import Registrarse from "./registrarse";
 import PostIcon from "@mui/icons-material/Book";
 import { PostCreate, PostEdit, PostList } from "./posts";
 import {DashboardList} from "./DashboardList";
-import {House, DynamicFeed} from '@mui/icons-material';
+import {Home, DynamicFeed,Description} from '@mui/icons-material';
 
 export const App = () => {
   return(
   <Admin dataProvider={dataProvider}>
-    <Resource name="dashboard" list={DashboardList} icon={House}/>
+    <Resource name="dashboard" list={DashboardList} icon={Home}/>
     <Resource name="users" list={UserList} />
     <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate} icon={DynamicFeed}/>
   </Admin>);
