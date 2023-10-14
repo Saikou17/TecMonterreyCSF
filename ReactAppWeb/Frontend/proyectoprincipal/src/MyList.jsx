c3c0f562ef7786e145b87af7e2dc3f04a7ae9bfd
angelafonso227 <angel.adrianafonso22@gmail.com>
2023-10-14 14:20:07 -0600

    cambios css

--- ReactAppWeb/Frontend/proyectoprincipal/src/MyList.jsx
+++ ReactAppWeb/Frontend/proyectoprincipal/src/MyList.jsx
@@ -105,5 +105,9 @@ const CardView = () => { //Componente que genera una carta
 export const CardList = (props) => (
   <List filters={CardFilters}>
     <CardView />
+    <div className="complete">
+      <h1>...................................................................................................................................................</h1>
+    </div>
   </List>
+  
 );
