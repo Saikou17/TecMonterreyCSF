2f1421d96f9904b792e1c65a0c6d92859f51f894
Saikou17 <a01783208@itesm.mx>
2023-10-18 10:23:20 -0600

    App Version 1.1 Beta

--- ReactAppWeb/Frontend/proyectoprincipal/src/Reportes.jsx
+++ ReactAppWeb/Frontend/proyectoprincipal/src/Reportes.jsx
@@ -12,67 +12,6 @@ import {
 import { Grid, Card, CardContent, CardHeader } from "@mui/material";
 /*import { report } from "process";*/
 
-const ReportFilter = [<DateInput source="Fecha" label="Seleccionar fecha" />];
-
-/*
-const ReportsView = () => {
-  const { data } = useListContext(); // Hook que guarda la información de los reportes
-
-  return (
-    <div>
-      {data?.map((report) => (
-        <Accordion key={report.Fecha}>
-          <Accordion.Item eventKey="0">
-            <Accordion.Header>
-              Fecha de reporte: {report.Fecha}
-            </Accordion.Header>
-            <Accordion.Body>
-              <h2>Reporte del: {report.Fecha}</h2>
-              <h3>Cantidad de Tickets: {report.cantidadTickets}</h3>
-              <h4>{report.Comentario}</h4>
-
-              <h5>Categorías por Ticket:</h5>
-              <p>Servicio: {report.Categorias.Servicios}</p>
-              <p>Digital: {report.Categorias.Digital}</p>
-              <p>Infraestructura: {report.Categorias.Infraestructura}</p>
-              <p>Beneficiarios: {report.Categorias.Beneficiarios}</p>
-              <p>Mobiliarios: {report.Categorias.Mobiliarios}</p>
-              <p>Seguridad: {report.Categorias.Seguridad}</p>
-              <p>Materiales: {report.Categorias.Materiales}</p>
-              <hr />
-              <h5>Prioridad Alta</h5>
-              <p>Cantidad de Tickets: {report.PrioridadTickets.Alta}</p>
-              <h5>Prioridad Intermedia</h5>
-              <p>Cantidad de Tickets: {report.PrioridadTickets.Intermedia}</p>
-              <h5>Prioridad Baja</h5>
-              <p>Cantidad de Tickets: {report.PrioridadTickets.Baja}</p>
-            </Accordion.Body>
-          </Accordion.Item>
-        </Accordion>
-      ))}
-    </div>
-  );
-};
-
-export const ReportsList = (props) => (
-  <List filters={ReportFilter}>
-    <ReportsView />
-  </List>
-);
-
-export const ReportCreate = () => {
-  const [isCreateFormOpen, setCreateFormOpen] = useState(false);
-
-  <Button variant="primary" onClick={() => setCreateFormOpen(true)}></Button>;
-  return (
-    <Create>
-      <SimpleForm>
-        <TextInput source="Fecha" label="Fecha" />
-      </SimpleForm>
-    </Create>
-  );
-};*/
-
 const ReportsView = () => {
   const { data } = useListContext(); // Hook que guarda la información de los reportes
   return (
@@ -144,22 +83,8 @@ const ReportsView = () => {
   );
 };
 
-export const ReportsList = (props: any) => (
+export const ReportsList = (props) => (
   <List /*filters={ReportFilter}*/>
     <ReportsView />
   </List>
 );
-
-/*
-export const ReportCreate = () => {
-  const [isCreateFormOpen, setCreateFormOpen] = useState(false);
-
-  <Button variant="primary" onClick={() => setCreateFormOpen(true)}></Button>;
-  return (
-    <Create>
-      <SimpleForm>
-        <TextInput source="Fecha" label="Fecha" />
-      </SimpleForm>
-    </Create>
-  );
-};*/
